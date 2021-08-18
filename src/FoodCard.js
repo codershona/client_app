72d75db46f5214fcefeb2686bd0f235220d91b65
codershona <falgunimst95@gmail.com>
2021-08-18 18:14:52 +0200

    Building my app and working in FoodCardList Component

--- src/FoodCard.js
+++ src/FoodCard.js
@@ -4,7 +4,7 @@ import './FoodCard.css';
 const FoodCard = ({restaurant_name, address, rating, image}) => {
     return (
         <div className="dtc tr tc grow bg-light-red br3 pa2 ma2 dib bw2 shadow-5">
-            <h1>Food Delivering to South Bermondsey</h1>
+            <h1>Food Items</h1>
             <img alt="food_items" src={`${image}`} width="200" height="200" />
             <div>
                 <h2>{ restaurant_name }</h2>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import FoodCard from './FoodCard';
+// import FoodCard from './FoodCard';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { foods } from './foods';
+import FoodCardList from './FoodCardList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-    <FoodCard id={foods[0].id} restaurant_name={foods[0].restaurant_name} address={foods[0].address} rating={foods[0].rating} image={foods[0].image} />
-    <FoodCard id={foods[1].id} restaurant_name={foods[1].restaurant_name} address={foods[1].address} rating={foods[1].rating} image={foods[1].image}/>
-    <FoodCard id={foods[2].id} restaurant_name={foods[2].restaurant_name} address={foods[2].address} rating={foods[2].rating} image={foods[2].image} />
-    </div>
+    <FoodCardList foods={foods} />
   </React.StrictMode>,
   document.getElementById('root')
 );
