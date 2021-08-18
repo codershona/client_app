e2b740c19f7b22257defac3fa44f945f5e94a33e
codershona <falgunimst95@gmail.com>
2021-08-18 18:17:04 +0200

    working in FoodCardList Component : removed the constant and added the concepts into the brackets

--- src/FoodCardList.js
+++ src/FoodCardList.js
@@ -2,21 +2,22 @@ import React from 'react';
 import FoodCard from './FoodCard';
 
 const FoodCardList = ({ foods }) => {
-    const foodcardComponent = foods.map((user, x) => {
-        return (
-        <FoodCard
-        key={x}
-        id={foods[x].id}
-        restaurant_name={foods[x].restaurant_name}
-        address={foods[x].address}
-        rating={foods[x].rating}
-        image={foods[x].image}
-        />
-        );
-    })
     return(
     <div>
-      { foodcardComponent }
+      {
+        foods.map((user, x) => {
+            return (
+            <FoodCard
+            key={x}
+            id={foods[x].id}
+            restaurant_name={foods[x].restaurant_name}
+            address={foods[x].address}
+            rating={foods[x].rating}
+            image={foods[x].image}
+            />
+            );
+        })
+      }
     </div>
     );
 };
