b3758b0859394cb65967bac1a80e830d237ccfce
codershona <falgunimst95@gmail.com>
2021-08-22 16:56:14 +0200

    lifecycle react: componentDidMount if we use any json api then we can fetch the json otherwise we can use setTimeout method to call the foods or setState only

--- src/App.js
+++ src/App.js
@@ -15,8 +15,14 @@ class App extends Component {
     }
 
     componentDidMount() {
+        setTimeout(() => {
+            this.setState({foods: foods})
+          }, 1000)
         // console.log('check');
-        this.setState({ foods: foods })
+        // If we use any json api then we can use this method
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => response.json())
+        // .then(users => this.setState({ foods: users }));
         // console.log('componentDidMount 2');
     }
 
@@ -27,17 +33,21 @@ class App extends Component {
         const filteredFoods = this.state.foods.filter(foods => {
             return foods.restaurant_name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if (this.state.foods.length === 0) {
+            return <h1> LOADING....</h1>
+        } else {
+            return (
+                <div className="tc">
+                <h1 className="f-subheadline">ARK Food Delivery Services</h1>
+                <SearchFoodBox searchChange={this.onSearchChange} />
+                <br/>
+                <FoodCardList foods={filteredFoods} />
+                </div>
+            );
+        }
         // console.log('render 3');
       //   console.log(event.target.value);
-      console.log(filteredFoods);
-        return (
-            <div className="tc">
-            <h1 className="f-subheadline">ARK Food Delivery Services</h1>
-            <SearchFoodBox searchChange={this.onSearchChange} />
-            <br/>
-            <FoodCardList foods={filteredFoods} />
-            </div>
-        );
+    //   console.log(filteredFoods);
     }
 }
 
